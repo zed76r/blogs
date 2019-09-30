a780547cb0be08ab5dc701f6860c8c4e720ceeb0
Zed <dev@zedd.dev>
2019-09-30 20:45:02 +0800

    remove old build method.

--- build.sh
+++
@@ -1,7 +0,0 @@
-npx hexo clean
-npx hexo generate
-rsync -av --delete --exclude=CNAME public/ docs/
-git add *
-git stage *
-datetime=`date +"%Y-%m-%dT%H:%M:%S"`
-git commit -m "rebuild at $datetime"
